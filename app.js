3d2ce9229a221e1065aadcfbc1e1db7ddd3cb195
Tiago Lopes <tiagolopes.imv@gmail.com>
2023-07-04 11:12:03 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando um sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+console.log ("consegui arruma uma grande cagada mas deu bom ")
